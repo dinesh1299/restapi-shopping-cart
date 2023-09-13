0766469392f57eab5e91c4a1f383acebbb04da10
Dinesh <dinesh.g@tvsnext.io>
2023-09-13 15:08:58 +0530

    Updated Project

--- first_app/api/urls.py
+++ first_app/api/urls.py
@@ -7,16 +7,19 @@ router.register('product',Product_ViewSet,basename="product")
 urlpatterns = [
     path('category_list', GetCategories.as_view(), name='category-list'),
     path('get_category/<int:id>',GetCategory.as_view(),name="category-detail"),
+
     path('',include(router.urls)),
+
     path('product_list', GetProducts.as_view(), name='product-list'),
     path('get_product/<int:id>',GetProduct.as_view(),name="product-detail"),
+
     # path('<int:pk>/order_list',OrderList.as_view(),name='order-list'),
     path('order_list',OrderList.as_view(),name='order-list'),
     # path('order_create/<int:pk>',OrderCreate.as_view(),name='order-create'),
     path('order_update/<int:pk>',OrderUpdate.as_view(),name='order-update'),
     path('order_create',OrderCreate.as_view(),name='ordercreate'),
-    
     path('get_order/<int:pk>',OrderDetail.as_view(),name='order-detail'),
+    
     path('rating_list',RatingList.as_view(),name='rating-list'),
     path('create_rating/<int:pk>',RatingCreate.as_view(),name="rating-create"),
     path('get_rating/<int:id>',RatingDetail.as_view(),name='rating-detail'),
@@ -26,4 +29,8 @@ urlpatterns = [
     path('product_search',ProductDetailSearch.as_view(), name='product-search'),
     path('product_ordering',ProductDetailOrdering.as_view(), name='product-ordering'),
 
+    path('throttle_rating_list',ThrottleRatingList.as_view(),name='t-rating-list'),
+    path('paging_rating_list',PageRatingList.as_view(),name='p-rating-list'),  
+    path('throttle_rating_deatil',ThrottleRatingDetail.as_view(),name='t-rating-detail'),
+    
 ] 
